014eba32fdfbcb86a0343e84caac5c33c955702e
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-09-21 10:28:50 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,4 +1,5 @@
 function ContactController() {
+
     this.contacts = [{
         name: 'Bob',
         phone: '0123458690'
@@ -10,9 +11,20 @@ function ContactController() {
         phone: '0684059433'
     }];
 
+    this.addContact = function () {
+        this.contacts.push({
+            name: this.name,
+            phone: this.phoneNumber
+        });
+        
+        this.name         = '';
+        this.phoneNumber  = '';
+    }
+
     this.removeContact = function (index) {
         this.contacts.splice(index, 1);
     }
+
 }
 
 angular
